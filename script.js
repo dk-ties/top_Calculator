a2d957a39d3deb04049d44819083333067ea587c
dk-ties <dk.ties@gmail.com>
2023-02-26 09:07:30 +0100

    setting up basic function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //Start of Calculation script.
-
+let currentNum = 0;
+let newNumber = 0;
 //Function for add
+function add(numOne, numTwo) {
+  currentNum = numOne + numTwo;
+  return currentNum;
+}
 //Function for subtract
 //Function for multiply
 //Function for divide
