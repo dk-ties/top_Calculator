10e952768c16a6d96bf4e0c1a67e1d855568ae52
dk-ties <dk.ties@gmail.com>
2023-02-26 09:41:14 +0100

    operatorDivide do not accept 0

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 //Start of Calculation script.
 let currentNum = 0;
-let newNumber = 0;
+let newNumber = 5;
+
+//currentNum = operatorAdd(currentNum, newNumber);
+
 //Function for add
-function add(numOne, numTwo) {
-  currentNum = numOne + numTwo;
+function operatorAdd(currentNum, newNumber) {
+  currentNum = currentNum + newNumber;
+  newNumber = 0;
   return currentNum;
 }
 //Function for subtract
+function operatorSubtract(currentNum, newNumber) {
+  currentNum = currentNum - newNumber;
+  newNumber = 0;
+  return currentNum;
+}
 //Function for multiply
+function operatorMultiply(currentNum, newNumber) {
+  currentNum = currentNum * newNumber;
+  newNumber = 0;
+  return currentNum;
+}
 //Function for divide
+function operatorDivide(currentNum, newNumber) {
+  if (currentNum <= 0 || newNumber <= 0) {
+    return alert("You cannot divide with 0");
+  }
+  currentNum = currentNum / newNumber;
+  newNumber = 0;
+  return currentNum;
+}
