2e2eb9e3bfa8c794b2a0d3393efd01af78805e24
dk-ties <dk.ties@gmail.com>
2023-02-27 10:07:58 +0100

    Setting up bool, for DivideZero, And currentNum in display. Also bugfixed issue with numA and numB not zeroed correct

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
 //Start of Calculation script.
 //declaration and reset of the two chosen numbers and the current opreator
 let currentNum = 0;
-let newNumber = 5;
+let newNumber = 0;
+let numA = 0;
+let numB = 0;
 let currentOperator = "";
 let operatorCall = "";
+let isDisplayCurrentNum = false;
+let isDivideWithZero = false;
 
-//Setting up btnNumber and listen event
+//Setting up btnNumber and listenevent
 let btnNumberClicked = document.querySelectorAll(".btnNumber");
 btnNumberClicked.forEach((element) =>
   element.addEventListener("click", (event) => {
-    let numberClick = event.target.innerHTML;
-    displayUpdate(numberClick);
+    // see if display is = current num
+    if (isDisplayCurrentNum == false && isDivideWithZero == false) {
+      let numberClick = event.target.innerHTML;
+      displayUpdate(numberClick);
+    } else {
+      isDivideWithZero = false;
+      isDisplayCurrentNum = false;
+      document.getElementById("inputDisplay").value = "";
+      let numberClick = event.target.innerHTML;
+      displayUpdate(numberClick);
+    }
   })
 );
 //Setting up btnOperator and listen event
 let btnOperatorClicked = document.querySelectorAll(".btnOperator");
 btnOperatorClicked.forEach((element) =>
   element.addEventListener("click", (event) => {
-    operatorCall = event.target.value;
+    currentOperator = event.target.value;
     btnOperatorClick();
+  })
+);
+//Setting up btnResult and listen event
+let btnResultClicked = document.querySelectorAll(".btnResult");
+btnResultClicked.forEach((element) =>
+  element.addEventListener("click", (event) => {
+    numB = Number(document.getElementById("inputDisplay").value);
+    btnResultClick();
+  })
+);
+
+//Setting up btnClear and listen event
+let btnClear = document.querySelectorAll(".btnClear");
+btnClear.forEach((element) =>
+  element.addEventListener("click", (event) => {
+    document.getElementById("inputDisplay").value = "";
+    numA = "";
+    numB = "";
+    currentOperator = "";
+    isDisplayCurrentNum = false;
+    isDivideWithZero = false;
   })
 );
 
@@ -32,18 +66,37 @@ function displayUpdate(numberClick) {
 //Function on btnOperator click
 function btnOperatorClick() {
   //newNumber = document.getElementById("inputDisplay").value;
-  operator(operatorCall);
-  if (operatorCall !== "result") {
+  //operator(operatorCall);
+  if (numA <= 0) {
+    numA = Number(document.getElementById("inputDisplay").value);
     document.getElementById("inputDisplay").value = "";
-  } else document.getElementById("inputDisplay").value = currentNum;
+  } else {
+    numB = Number(document.getElementById("inputDisplay").value);
+    btnResultClick();
+  }
+}
+
+//Function on btnResult click
+function btnResultClick() {
+  //newNumber = document.getElementById("inputDisplay").value;
+  operator(currentOperator);
+  numA = currentNum;
+  numB = "";
+  currentOperator = "";
+  if (isDivideWithZero == true) {
+    currentNum = "";
+    document.getElementById("inputDisplay").value = "Error";
+  } else {
+    document.getElementById("inputDisplay").value = currentNum;
+    isDisplayCurrentNum = true;
+  }
 }
 
 //main operator
 function operator(exspression) {
   switch (exspression) {
     case "operatorAdd":
-      newNumber = Number(document.getElementById("inputDisplay").value);
-      currentNum = Number(operatorAdd());
+      currentNum = operatorAdd();
       return currentNum;
 
     case "operatorSubtract":
@@ -66,31 +119,30 @@ function operator(exspression) {
 //Functions for the chosen operators
 //Function for add
 function operatorAdd() {
-  currentNum = currentNum + newNumber;
-  newNumber = 0;
-  parseInt(currentNum);
+  currentNum = numA + numB;
+
   return currentNum;
 }
 //Function for subtract
 function operatorSubtract() {
-  currentNum = currentNum - newNumber;
-  newNumber = 0;
+  currentNum = numA - numB;
   return currentNum;
 }
 //Function for multiply
 function operatorMultiply() {
-  currentNum = currentNum * newNumber;
-  newNumber = 0;
+  currentNum = numA * numB;
   return currentNum;
 }
 //Function for divide
 function operatorDivide() {
-  if (currentNum <= 0 || newNumber <= 0) {
-    console.log(newNumber);
-    console.log(currentNum);
-    return alert("You cannot divide with 0");
+  if (numA <= 0 || numB <= 0) {
+    console.log(numA);
+    console.log(numB);
+    isDivideWithZero = true;
+    currentNum = "";
+    return currentNum;
+    //return alert("You cannot divide with 0");
   }
-  currentNum = currentNum / newNumber;
-  newNumber = 0;
+  currentNum = numA / numB;
   return currentNum;
 }
